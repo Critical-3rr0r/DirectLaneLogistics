68b4d528470e84a6cc08ff52d583f5ab8f13eacd
Critical-3rr0r <dalton.robinson36@gmail.com>
2026-04-10 00:00:29 -0500

    FIX: adjusted main images to be eager loaded to avoid pop-in on initial page load

--- app/components/logo.tsx
+++ app/components/logo.tsx
@@ -5,7 +5,7 @@ export default function Logo(){
     return(
     <div className="self-center mt-5 bg-linear-to-b dark:from-gray-900/10 dark:to-gray-900 pb-10 w-full flex flex-col items-center">
         <motion.div className="z-3" initial={{opacity: 0}} animate={{opacity: 100}} transition={{duration: 1.5}}>
-            <Image src={"/images/DirectLaneLogo.png"} width={750} height={750} alt="company logo"/>
+            <Image src={"/images/DirectLaneLogo.png"} width={750} height={750} priority alt="company logo"/>
         </motion.div>
     </div>
 
--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
     <div className="flex flex-col min-h-screen items-center bg-zinc-50 font-sans dark:bg-gray-950">
       <Logo />
       <div className="w-full dark:bg-gray-900 flex flex-col pt-10 z-1">
-        <Image src={"/images/Truck.png"} height={500} width={1500} alt="Truck Background" objectFit="cover" className="-z-1 w-full h-350 self-center mask-b-from-0% mask-b-to-100% mask-t-from-25% mask-t-to-100% top-0 absolute"/>
+        <Image src={"/images/Truck.png"} height={500} width={1500} priority alt="Truck Background" objectFit="cover" className="-z-1 w-full h-350 self-center mask-b-from-0% mask-b-to-100% mask-t-from-25% mask-t-to-100% top-0 absolute"/>
         <Hero />
         <div className="flex flex-col gap-10 mt-35">
           <h2 className="text-6xl text-center pb-10">What you get with <span className="text-orange-600 font-bold">DirectLane</span></h2>
